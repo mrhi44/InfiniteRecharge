8f5e4c3bd6f18926671ed9e9df187f97e0dcf093
Jordan Bancino <jordan@bancino.net>
2020-01-26 16:31:50 -0500

    Move Constants

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -28,6 +28,28 @@ import net.bancino.robotics.swerveio.log.csv.CSVPIDSwerveLogger;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveTrain extends SwerveDrive {
+
+  /*
+     * Motors - CAN IDs
+     */
+    public static final int FRONT_RIGHT_DRIVE_MOTOR = 5; /* Module 1 */
+    public static final int FRONT_LEFT_DRIVE_MOTOR = 6; /* Module 2 */
+    public static final int REAR_LEFT_DRIVE_MOTOR = 7; /* Module 3 */
+    public static final int REAR_RIGHT_DRIVE_MOTOR = 8; /* Module 4 */
+
+    public static final int FRONT_RIGHT_PIVOT_MOTOR = 1; /* Module 1 */
+    public static final int FRONT_LEFT_PIVOT_MOTOR = 2; /* Module 2 */
+    public static final int REAR_LEFT_PIVOT_MOTOR = 3; /* Module 3 */
+    public static final int REAR_RIGHT_PIVOT_MOTOR = 4; /* Module 4 */
+
+    /*
+     * Encoders - Analog Ports
+     */
+    public static final int FRONT_RIGHT_ANALOG_ENCODER = 0; /* Module 1 */
+    public static final int FRONT_LEFT_ANALOG_ENCODER = 1; /* Module 2 */
+    public static final int REAR_LEFT_ANALOG_ENCODER = 2; /* Module 3 */
+    public static final int REAR_RIGHT_ANALOG_ENCODER = 3; /* Module 4 */
+
   /* These are in centimeters, but unit really doesn't matter. */
   public static final double BASE_WIDTH = 29;
   public static final double BASE_LENGTH = 29;
@@ -35,15 +57,15 @@ public class DriveTrain extends SwerveDrive {
   public static final double COUNTS_PER_PIVOT_REVOLUTION = 360;
 
   /* The encoder offsets tell us where zero is for each motor. */
-  public static final double FRONT_RIGHT_ENCODER_OFFSET = 52.9;
-  public static final double FRONT_LEFT_ENCODER_OFFSET = 195.1;
-  public static final double REAR_LEFT_ENCODER_OFFSET = 350.8;
-  public static final double REAR_RIGHT_ENCODER_OFFSET = 328.35;
+  public static final double FRONT_RIGHT_ENCODER_OFFSET = 54.65;
+  public static final double FRONT_LEFT_ENCODER_OFFSET = 196.27;
+  public static final double REAR_LEFT_ENCODER_OFFSET = 351.45;
+  public static final double REAR_RIGHT_ENCODER_OFFSET = 160.66;
 
-  private static AnalogEncoder frontRightEncoder = new AnalogEncoder(Constants.FRONT_RIGHT_ANALOG_ENCODER, FRONT_RIGHT_ENCODER_OFFSET);
-  private static AnalogEncoder frontLeftEncoder = new AnalogEncoder(Constants.FRONT_LEFT_ANALOG_ENCODER, FRONT_LEFT_ENCODER_OFFSET);
-  private static AnalogEncoder rearLeftEncoder = new AnalogEncoder(Constants.REAR_LEFT_ANALOG_ENCODER, REAR_LEFT_ENCODER_OFFSET);
-  private static AnalogEncoder rearRightEncoder = new AnalogEncoder(Constants.REAR_RIGHT_ANALOG_ENCODER, REAR_RIGHT_ENCODER_OFFSET);
+  private static AnalogEncoder frontRightEncoder = new AnalogEncoder(FRONT_RIGHT_ANALOG_ENCODER, FRONT_RIGHT_ENCODER_OFFSET);
+  private static AnalogEncoder frontLeftEncoder = new AnalogEncoder(FRONT_LEFT_ANALOG_ENCODER, FRONT_LEFT_ENCODER_OFFSET);
+  private static AnalogEncoder rearLeftEncoder = new AnalogEncoder(REAR_LEFT_ANALOG_ENCODER, REAR_LEFT_ENCODER_OFFSET);
+  private static AnalogEncoder rearRightEncoder = new AnalogEncoder(REAR_RIGHT_ANALOG_ENCODER, REAR_RIGHT_ENCODER_OFFSET);
 
   /**
    * Create the SwerveDrive with the default settings and the robot map.
@@ -52,13 +74,13 @@ public class DriveTrain extends SwerveDrive {
     super(BASE_WIDTH, BASE_LENGTH, COUNTS_PER_PIVOT_REVOLUTION, () -> {
       var modules = new HashMap<SwerveModule, AbstractSwerveModule>();
       modules.put(SwerveModule.FRONT_RIGHT,
-          new MK2SwerveModule(Constants.FRONT_RIGHT_DRIVE_MOTOR, Constants.FRONT_RIGHT_PIVOT_MOTOR, frontRightEncoder));
+          new MK2SwerveModule(FRONT_RIGHT_DRIVE_MOTOR, FRONT_RIGHT_PIVOT_MOTOR, frontRightEncoder));
       modules.put(SwerveModule.FRONT_LEFT,
-          new MK2SwerveModule(Constants.FRONT_LEFT_DRIVE_MOTOR, Constants.FRONT_LEFT_PIVOT_MOTOR, frontLeftEncoder));
+          new MK2SwerveModule(FRONT_LEFT_DRIVE_MOTOR, FRONT_LEFT_PIVOT_MOTOR, frontLeftEncoder));
       modules.put(SwerveModule.REAR_LEFT,
-          new MK2SwerveModule(Constants.REAR_LEFT_DRIVE_MOTOR, Constants.REAR_LEFT_PIVOT_MOTOR, rearLeftEncoder));
+          new MK2SwerveModule(REAR_LEFT_DRIVE_MOTOR, REAR_LEFT_PIVOT_MOTOR, rearLeftEncoder));
       modules.put(SwerveModule.REAR_RIGHT,
-          new MK2SwerveModule(Constants.REAR_RIGHT_DRIVE_MOTOR, Constants.REAR_RIGHT_PIVOT_MOTOR, rearRightEncoder));
+          new MK2SwerveModule(REAR_RIGHT_DRIVE_MOTOR, REAR_RIGHT_PIVOT_MOTOR, rearRightEncoder));
       return modules; /* Return the module map for the constructor's use. */
     }, (module) -> {
       module.getPivotPIDController().setOutputRampRate(0);
@@ -68,7 +90,7 @@ public class DriveTrain extends SwerveDrive {
 
     zeroDriveEncoders();
 
-    setIdleAngle(0, false);
+    //setIdleAngle(0, false);
 
     startLogging(new DashboardSwerveLogger());
 
