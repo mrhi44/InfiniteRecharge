c6c011f3fbd1e36d16abbf945c6e93311287cf34
snyderethan <snyderethan@waylandunion.org>
2020-02-20 18:52:59 -0500

    Fixed to work with conventions and all

--- src/main/java/frc/robot/commands/ColorWheelDebug.java
+++ src/main/java/frc/robot/commands/ColorWheelDebug.java
@@ -7,19 +7,21 @@ import edu.wpi.first.wpilibj.util.Color;
 
 import com.revrobotics.ColorSensorV3;
 
-
 public class ColorWheelDebug extends CommandBase {
+
+    I2C.Port i2cPort;
+    ColorSensorV3 colorSensorV3;
     String colorString;
     Boolean colorFound;
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
-    public ColorWheelDebug(){
+    public ColorWheelDebug(I2C.Port i2cPort, ColorSensorV3 colorSensorV3){
+        this.i2cPort = i2cPort;
+        this.colorSensorV3 = colorSensorV3;
     }
 
     public void execute() {
-        Color detectedColor = m_colorSensor.getColor();
-        int proximity = m_colorSensor.getProximity();
+        Color detectedColor = colorSensorV3.getColor();
+        int proximity = colorSensorV3.getProximity();
 
         SmartDashboard.putNumber("Red", detectedColor.red);
         SmartDashboard.putNumber("Green", detectedColor.green);
