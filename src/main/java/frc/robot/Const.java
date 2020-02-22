3ea96217c9d7669d71c243e5fdfebb43ec909bf5
Jordan Bancino <jordan@bancino.net>
2020-02-21 22:47:07 -0500

    Swerve CAN is good

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -6,14 +6,14 @@ package frc.robot;
 public class Const {
     public static class CAN {
         public static final int FRONT_RIGHT_DRIVE_MOTOR = 5; /* Module 1 */
-        public static final int FRONT_LEFT_DRIVE_MOTOR  = 6; /* Module 2 */
-        public static final int REAR_LEFT_DRIVE_MOTOR   = 7; /* Module 3 */
-        public static final int REAR_RIGHT_DRIVE_MOTOR  = 8; /* Module 4 */
+        public static final int FRONT_LEFT_DRIVE_MOTOR  = 2; /* Module 2 */
+        public static final int REAR_LEFT_DRIVE_MOTOR   = 3; /* Module 3 */
+        public static final int REAR_RIGHT_DRIVE_MOTOR  = 4; /* Module 4 */
 
         public static final int FRONT_RIGHT_PIVOT_MOTOR = 1; /* Module 1 */
-        public static final int FRONT_LEFT_PIVOT_MOTOR  = 2; /* Module 2 */
-        public static final int REAR_LEFT_PIVOT_MOTOR   = 3; /* Module 3 */
-        public static final int REAR_RIGHT_PIVOT_MOTOR  = 4; /* Module 4 */
+        public static final int FRONT_LEFT_PIVOT_MOTOR  = 6; /* Module 2 */
+        public static final int REAR_LEFT_PIVOT_MOTOR   = 7; /* Module 3 */
+        public static final int REAR_RIGHT_PIVOT_MOTOR  = 8; /* Module 4 */
 
         public static final int ELEVATOR_MOTOR = 15;
         public static final int ELEVATOR_WHEEL = 16;
