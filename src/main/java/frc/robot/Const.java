ffe3752279138ffc79c61d57fc8256f90f1357b2
Jordan Bancino <jordan@bancino.net>
2020-02-28 18:52:31 -0500

    Clean up Drivetrain & Update Robot Map

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -9,12 +9,12 @@ public class Const {
      * CAN IDs of all the motor controllers, the PCM and PDP are here.
      */
     public static class CAN {
-        public static final int FRONT_RIGHT_DRIVE_MOTOR = 5; /* Module 1 */
+        public static final int FRONT_RIGHT_DRIVE_MOTOR = 1; /* Module 1 */
         public static final int FRONT_LEFT_DRIVE_MOTOR  = 2; /* Module 2 */
         public static final int REAR_LEFT_DRIVE_MOTOR   = 3; /* Module 3 */
         public static final int REAR_RIGHT_DRIVE_MOTOR  = 4; /* Module 4 */
 
-        public static final int FRONT_RIGHT_PIVOT_MOTOR = 1; /* Module 1 */
+        public static final int FRONT_RIGHT_PIVOT_MOTOR = 5; /* Module 1 */
         public static final int FRONT_LEFT_PIVOT_MOTOR  = 6; /* Module 2 */
         public static final int REAR_LEFT_PIVOT_MOTOR   = 7; /* Module 3 */
         public static final int REAR_RIGHT_PIVOT_MOTOR  = 8; /* Module 4 */
