795b994fd2e125321625d25dc78125d2b10f2356
Jordan Bancino <jordan@bancino.net>
2021-01-19 19:36:30 -0500

    Fix compile error in ThreeCellAutonomous

--- src/main/java/frc/robot/commands/auton/ThreeCellAutonomous.java
+++ src/main/java/frc/robot/commands/auton/ThreeCellAutonomous.java
@@ -24,36 +24,31 @@ import net.bancino.robotics.swerveio.command.PathweaverSwerveDrive.PathExecution
 import net.bancino.robotics.jlimelight.Limelight;
 
 /**
- * This command runs an autonomous mode that simply scores the three pre-loaded 
- * power cells. Nothing more, nothing less. It supports any path, just pass in the
- * path name to the constructor.
+ * This command runs an autonomous mode that simply scores the three pre-loaded
+ * power cells. Nothing more, nothing less. It supports any path, just pass in
+ * the path name to the constructor.
  *
  * @author Jordan Bancino
  */
 public class ThreeCellAutonomous extends SequentialCommandGroup {
-  
-  private static final double feedWithShooterSpeed = RobotContainer.config().getDouble("feedWithShooterSpeed"));
 
-  public ThreeCellAutonomous(String path, SwerveDrive swerve, Shooter shooter, Intake intake, Feed feed, Limelight limelight) throws IOException {
-    super(
-      new InstantCommand(() -> {
-        shooter.run();
-        intake.lift(true);
-      }, shooter, intake),
-      new PathweaverSwerveDrive(swerve, "paths/output/" + path + ".wpilib.json", PathExecutionMode.ROBOT_BACKWARDS),
-      new LimelightAlign(swerve, limelight, shooter, true, 1000),
-      new ParallelCommandGroup(
-        new ShooterHoodWithLimelight(shooter, limelight),
-        new SequentialCommandGroup(
-          new WaitCommand(0.5),
-          new InstantCommand(() -> feed.runAt(feedWithShooterSpeed, feed),
-          new WaitCommand(4),
-          new InstantCommand(() -> {
-            shooter.stop();
-            feed.stop();
-          }, feed)  /* We don't require the shooter here because the hood command is using it. */
-        )
-      )
-    );
+  private static final double feedWithShooterSpeed = RobotContainer.config().getDouble("feedWithShooterSpeed");
+
+  public ThreeCellAutonomous(String path, SwerveDrive swerve, Shooter shooter, Intake intake, Feed feed,
+      Limelight limelight) throws IOException {
+    super(new InstantCommand(() -> {
+      shooter.run();
+      intake.lift(true);
+    }, shooter, intake),
+        new PathweaverSwerveDrive(swerve, "paths/output/" + path + ".wpilib.json", PathExecutionMode.ROBOT_BACKWARDS),
+        new LimelightAlign(swerve, limelight, shooter, true, 1000),
+        new ParallelCommandGroup(new ShooterHoodWithLimelight(shooter, limelight),
+            new SequentialCommandGroup(new WaitCommand(0.5),
+                new InstantCommand(() -> feed.runAt(feedWithShooterSpeed), feed), new WaitCommand(4),
+                new InstantCommand(() -> {
+                  shooter.stop();
+                  feed.stop();
+                }, feed) /* We don't require the shooter here because the hood command is using it. */
+            )));
   }
 }
